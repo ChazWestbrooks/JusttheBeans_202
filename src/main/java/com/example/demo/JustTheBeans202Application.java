d10ff46f2239e400d8491e463d41eba251eaa36d
Chaz Westbrooks <Chaz@Chazs-MacBook-Pro.local>
2018-10-08 16:25:20 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/JustTheBeans202Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustTheBeans202Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustTheBeans202Application.class, args);
+    }
+}
